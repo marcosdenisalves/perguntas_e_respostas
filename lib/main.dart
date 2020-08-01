4c155cfa92258b88ae9c6d29440acabd1c75a91c
Marcos Dênis <dee.alves2@gmail.com>
2020-08-01 11:47:43 -0300

    Projeto criado

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter'),
+    );
+  }
+}
